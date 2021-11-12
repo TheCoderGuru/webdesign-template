1ddc6a55bb201775490833ce36eb080300b8d02c
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2021-11-12 11:17:29 -0400

    Updated the files

---
+++ dist/index.6c907e24.js
@@ -0,0 +1,12 @@
+// Read more about JQuery on the docs here https://devdocs.io/jquery/
+$(document).ready(function() {
+    $(".button").hover(function() {
+        $(".button").css("background-color", "darkblue", 0.2);
+    });
+    $(".button").mouseleave(function() {
+        $(".button").css("background-color", "#3d5af1");
+    });
+    document.body.style.overflow = "hidden";
+});
+
+//# sourceMappingURL=index.6c907e24.js.map
